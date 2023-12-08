ccd977c81bfe2fd08a1604647918d72bcb3070d4
Crowdbotics <team@crowdbotics.com>
2023-12-08 21:07:07 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wasa-44928",
+  url: "https://wasa-44928.botics.co",
+  api: "https://wasa-44928.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
